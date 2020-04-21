88d58f60fa51da71366f5847f4e9413fe60b1817
Justin Starks <js2793@cornell.edu>
2020-04-21 17:36:44 -0400

    Add main file to project

---
+++ main.ml
@@ -0,0 +1,6 @@
+(* [play_game] starts the chess game. It asks for the players' names, asks 
+   which player is white and which is black, initilizes the board, and allows
+   the players to play *)
+let play_game =
+  failwith "Unimplemented"
+
