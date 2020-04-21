32b765173422edbda5e46e175cf80fcf6e79f374
Justin Starks <js2793@cornell.edu>
2020-04-21 18:16:36 -0400

    Finish documentation for state

--- state.mli
+++ state.mli
@@ -9,20 +9,31 @@
 (** The abstract type of values representing the game state. *)
 type t
 
+(** The type representing the people playing the game *)  
+type person = string
+
+
+
+(** [init_state g] is the initial state of the the chess game [g]. In this 
+    state, the game is set up as a normal chessboard, with white being the 
+    person to be able to make the first move. *)
 val init_state : Chessboard.t -> t
 
-val turn : t 
+(* [turn s] is the turn of the player who needs to move next in state [s] *)
+val turn : t -> person
 
-val player_name: t
+(* [player_name p] is the name of the player [p] *)
+val player_name: person
 
-val score : t -> int
+(* [score p s] is the current score of person [p] in state [s] as calculated by
+    the chess piece relative value system *)
+val score : t -> person -> int
 
-val current_board: t
+(* [current_board s] is the current board represented by state [s] *)
+val current_board: t -> Chessboard.t
 
-val take_piece : t -> t
-
-val valid_move: t -> bool
-
+(* [check s] determines whether the current state [s] has a player in check *)
 val check : t -> bool
 
+(* [check s] determines whether the current state [s] has a player in checkmate *)
 val checkmate: t -> bool
