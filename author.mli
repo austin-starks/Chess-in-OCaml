d7e896d4ad238b04c630155b191a41da214e1f55
Justin Starks <js2793@cornell.edu>
2020-05-09 14:14:59 -0400

    Included authors and install instructions

---
+++ author.mli
@@ -0,0 +1,7 @@
+(**
+   Metadata about this submission.
+   @authors Justin Starks (js2793) and Austin Starks (as2779). 
+*)
+
+(** [hours_worked] is the number of hours you worked on this assignment. *)
+val hours_worked : int
