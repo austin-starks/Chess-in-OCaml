2136537934a6430126800f88a0b0960a4d26994d
Austin Starks <as2779@cornell.edu>
2020-05-08 22:12:17 -0400

    Mege branch 'master'

--- chessboard.ml
+++ chessboard.ml
@@ -257,7 +257,7 @@ let rec rook_path_blocked t start_pos end_pos =
 let path_is_blocked t piece start_pos end_pos = 
   match piece with 
   | None -> raise NotAPiece
-  | Pawn color ->  false
+  | Pawn color ->  if (start_pos.letter = end_pos.letter && get_piece t end_pos <> None) then true else false
   (* if (get_piece t end_pos = None && 
                        abs (end_pos.number - start_pos.number) = 1) ||
                       (abs (end_pos.number - start_pos.number) = 2 &&
@@ -278,8 +278,8 @@ let is_valid_move t piece pos1 pos2 =
   || path_is_blocked t piece pos1 pos2 
   then false else match piece with 
     | Pawn Black-> if 
-      (pos1.letter = pos2.letter && get_piece t pos2 = None &&
-       (pos2.number = pos1.number -1 || pos1.number = 7 && pos2.number = 5)) 
+      (
+        (pos2.number = pos1.number -1 || pos1.number = 7 && pos2.number = 5)) 
       ||
       (pos2.number = pos1.number -1 && 
        List.assoc pos2.letter pos_letter_assoc_list - 
@@ -293,8 +293,8 @@ let is_valid_move t piece pos1 pos2 =
        | _ -> false)
       then true else false
     | Pawn White -> if 
-      (pos2.letter = pos1.letter && get_piece t pos2 = None && 
-       (pos2.number = pos1.number +1 || pos1.number = 2 && pos2.number = 4)) 
+      (
+        (pos2.number = pos1.number +1 || pos1.number = 2 && pos2.number = 4)) 
       || 
       (pos2.number = pos1.number +1 && 
        List.assoc pos2.letter pos_letter_assoc_list - 
@@ -333,10 +333,46 @@ let is_valid_move t piece pos1 pos2 =
                       List.assoc pos2.letter pos_letter_assoc_list + 2) 
       then true else false 
     | Bishop _ -> is_bishop_move piece pos1 pos2
-    | King _ -> (is_bishop_move piece pos1 pos2 || is_rook_move piece pos1 pos2)
-                && List.assoc pos1.letter pos_letter_assoc_list  - 
-                   List.assoc pos2.letter pos_letter_assoc_list |> Int.abs <=1
-                && pos1.number - pos2.number |> Int.abs <=1
+    | King color -> ((is_bishop_move piece pos1 pos2 || is_rook_move piece pos1 pos2)
+                     && List.assoc pos1.letter pos_letter_assoc_list  - 
+                        List.assoc pos2.letter pos_letter_assoc_list |> Int.abs <=1
+                     && pos1.number - pos2.number |> Int.abs <=1) 
+                    || 
+                    (get_piece t pos1 = King color && 
+                     get_piece t {letter = "F"; number = 1} = None && 
+                     get_piece t {letter = "G"; number = 1} = None && 
+                     get_piece t {letter = "H"; number = 1} = Rook color &&
+                     List.assoc pos1.letter pos_letter_assoc_list  = 
+                     List.assoc pos2.letter pos_letter_assoc_list - 2
+                    ) 
+                    ||
+                    (get_piece t pos1 = King color && 
+                     get_piece t {letter = "F"; number = 8} = None && 
+                     get_piece t {letter = "G"; number = 8} = None && 
+                     get_piece t {letter = "H"; number = 8} = Rook color &&
+                     List.assoc pos1.letter pos_letter_assoc_list  = 
+                     List.assoc pos2.letter pos_letter_assoc_list - 2
+
+                    ) 
+                    ||
+                    (get_piece t pos1 = King color && 
+                     get_piece t {letter = "D"; number = 1} = None && 
+                     get_piece t {letter = "C"; number = 1} = None && 
+                     get_piece t {letter = "B"; number = 1} = None && 
+                     get_piece t {letter = "A"; number = 1} = Rook color  &&
+                     List.assoc pos1.letter pos_letter_assoc_list  = 
+                     List.assoc pos2.letter pos_letter_assoc_list + 2
+                    ) 
+                    ||
+                    (get_piece t pos1 = King color && 
+                     get_piece t {letter = "D"; number = 8} = None && 
+                     get_piece t {letter = "C"; number = 8} = None && 
+                     get_piece t {letter = "B"; number = 8} = None && 
+                     get_piece t {letter = "A"; number = 8} = Rook color &&
+                     List.assoc pos1.letter pos_letter_assoc_list  = 
+                     List.assoc pos2.letter pos_letter_assoc_list + 2
+                    ) 
+
     | Queen _ -> is_bishop_move piece pos1 pos2 || is_rook_move piece pos1 pos2
     | Rook _ -> is_rook_move piece pos1 pos2
     | None -> false
@@ -347,7 +383,6 @@ let move_piece t pos1 pos2 =
   let position2 = parse_position pos2 in  
   let piece_to_move = 
     get_piece t position1 in
-  (* TODO - say is_valid_move || is_valid_pawn_move *)
   if is_valid_move t piece_to_move position1 position2 
   then
     let pos1_letter_index = 
@@ -360,6 +395,20 @@ let move_piece t pos1 pos2 =
     let _ = check_piece_color piece_to_move piece_at_loc in 
     chess_row_pos2.(pos2_letter_index) <- piece_to_move;
     chess_row_pos1.(pos1_letter_index) <- None; 
+    match piece_to_move with 
+    | King color ->  if 
+      List.assoc position1.letter pos_letter_assoc_list - 
+      List.assoc position2.letter pos_letter_assoc_list = 2
+      then 
+        let _ = chess_row_pos1.(0) <- None in 
+        chess_row_pos1.(3) <- Rook color
+      else if 
+        List.assoc position1.letter pos_letter_assoc_list - 
+        List.assoc position2.letter pos_letter_assoc_list = -2
+      then 
+        let _ = chess_row_pos1.(7) <- None in 
+        chess_row_pos1.(5) <- Rook color
+    | _ -> ()
   else raise IllegalMoveError
 
 let piece_to_string piece = 
@@ -395,21 +444,21 @@ let count_pieces t color =
   let knight_count = ref 0 in 
   List.iter (
     fun row -> Array.iter (fun piece -> match piece with 
-      | Rook clr ->
-         if piece_color piece = color then rook_count := !rook_count + 1
-      | Pawn clr ->
-         if piece_color piece = color then pawn_count := !pawn_count +  1
-      | Bishop clr ->
-         if piece_color piece = color then bishop_count := !bishop_count +  1
-      | Knight clr ->
-         if piece_color piece = color then knight_count := !knight_count +  1
-      | King clr ->
-         if piece_color piece = color then king_count := !king_count +  1
-      | Queen clr ->
-         if piece_color piece = color then queen_count := !queen_count +  1
-      | None ->
-         ()
-  ) row) t;
+        | Rook clr ->
+          if piece_color piece = color then rook_count := !rook_count + 1
+        | Pawn clr ->
+          if piece_color piece = color then pawn_count := !pawn_count +  1
+        | Bishop clr ->
+          if piece_color piece = color then bishop_count := !bishop_count +  1
+        | Knight clr ->
+          if piece_color piece = color then knight_count := !knight_count +  1
+        | King clr ->
+          if piece_color piece = color then king_count := !king_count +  1
+        | Queen clr ->
+          if piece_color piece = color then queen_count := !queen_count +  1
+        | None ->
+          ()
+      ) row) t;
   [
     ("King", !king_count);
     ("Queen", !queen_count);
